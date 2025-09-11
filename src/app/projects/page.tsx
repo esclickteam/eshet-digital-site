802554727c0fda94f2a377cef4f216127dfff83d
esclickteam <your-email@example.com>
2025-09-11 16:16:47 +0300

    תיאור ברור של מה שעדכנת

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import Projects from "../../../components/Projects";
 export default function ProjectsPage({
   searchParams,
 }: {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams: Record<string, string | string[] | undefined>;
 }) {
   const category = (searchParams?.category as string) || null;
 
