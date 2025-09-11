f038c3061e7aa1a73079eee94f2885902f202663
esclickteam <your-email@example.com>
2025-09-11 16:35:50 +0300

    תיאור ברור של מה שעדכנת

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Hero from "./Hero";
 import AboutSection from "./AboutSection";
 import Services from "./Services";
 import ValuesSection from "./ValuesSection";
-import Projects from "../../components/Projects";
 import StatsSection from "../../components/StatsSection"; 
 import GetStartedForm from "../../components/GetStartedForm";
+
 export default function Home() {
   return (
     <>
@@ -16,10 +16,8 @@ export default function Home() {
         <Hero />
         <AboutSection />
         <Services />
-
         <ValuesSection />
         <StatsSection />   
-        <Projects />
         <GetStartedForm />
         {/* Footer יורד מכאן כי הוא כבר קיים ב-layout */}
       </main>
