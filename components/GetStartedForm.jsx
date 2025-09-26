32b13d1cb79bc59864e4bedfcc6180ff1aab1caf
esclickteam <your-email@example.com>
2025-09-26 18:20:21 +0300

    תיאור ברור של מה שעדכנת

--- components/GetStartedForm.jsx
+++ components/GetStartedForm.jsx
@@ -157,6 +157,7 @@ export default function GetStartedForm() {
               preferredCountries={["us", "gb", "de", "fr", "it", "es", "nl", "il"]}
               enableSearch={true}
               countryCodeEditable={false} // ✅ לא מאפשר למחוק את הקידומת
+              separateDialCode={true} 
               value={phone}
               onChange={(val) => setPhone("+" + val)} // ✅ אין כפל קידומת
               inputProps={{
